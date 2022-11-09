0d6929e0356f5d12267c9655359faea6f980e316
irinaandonova <irinaandonova97@gmail.com>
2022-11-09 15:54:39 +0200

    Add derived class Hiking trail

---
+++ HikingBlog/HikingBlog/HikingTrail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HikingBlog
+{
+    public class HikingTrail: Destination
+    {
+        private int Difficulty;
+        private int HikingDuration;
+        public HikingTrail(string name, string creator, string description, string imageUrl, string region, int difficulty, int HikingDuration): base(name, creator, description, imageUrl, region)
+        {
+            if (difficulty == 1 || difficulty == 2 || difficulty == 3)
+                Difficulty = difficulty;
+            else
+                throw new Exception("Difficulty value must be between 1 and 3");
+        }
+
+    }
+}
