280a139b28c6558359e3f9a32ebb5da302dc0e7a
irinaandonova <irinaandonova97@gmail.com>
2022-11-09 15:24:22 +0200

    Add Park class

---
+++ HikingBlog/HikingBlog/Park.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HikingBlog
+{
+    public class Park: Destination
+    {
+        private bool HasPlayground;
+        private bool IsDogFriendlyl;
+        public Park(string name, string creator, string description, string imageUrl, string region, bool hasPlayground, bool isDogFriendlyl) : base(name, creator, description, imageUrl, region)
+        {
+            HasPlayground = hasPlayground;
+            IsDogFriendlyl = isDogFriendlyl;
+        }
+    }
+}
