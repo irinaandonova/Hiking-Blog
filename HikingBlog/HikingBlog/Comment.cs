f808ac90aa7f9fc0ed9167e1d985da552379a660
irinaandonova <irinaandonova97@gmail.com>
2022-11-09 13:29:44 +0200

    Add Comment class

---
+++ HikingBlog/HikingBlog/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HikingBlog
+{
+    public class Comment
+    {
+        public User Creator;
+        public Destination Destination;
+        public string Text;
+        public DateTime Date;
+
+        public Comment(User creator, Destination destination, string text)
+        {
+            Creator = creator;
+            Destination = destination;
+            Text = text;
+            Date = DateTime.Now;
+        }
+    }
+}
