6b0f28dae0fcde09f41f4ede88d06be792f5e334
irinaandonova <irinaandonova97@gmail.com>
2022-11-09 15:47:08 +0200

    Add derived class Seaside

---
+++ HikingBlog/HikingBlog/Seaside.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HikingBlog
+{
+    public class Seaside: Destination
+    {
+        private bool IsGuarded;
+        private bool OffersUmbrella;
+        private double UmbrellaPrice;
+        
+        public Seaside(string name, string creator, string description, string imageUrl, string region, bool isGuarded, bool offersUmbrella) : base(name, creator, description, imageUrl, region)
+        {
+            IsGuarded = isGuarded;
+            OffersUmbrella = offersUmbrella;
+        }
+        public void AddUmbrellaPrices(double umbrellaPrice)
+        {
+            if (!OffersUmbrella)
+                OffersUmbrella = true;
+            UmbrellaPrice = umbrellaPrice;            
+        }
+    }
+}
