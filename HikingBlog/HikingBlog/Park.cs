0bdad49f47907e5d5bee8a713a913e3b690fd7de
irinaandonova <irinaandonova97@gmail.com>
2022-11-11 11:22:11 +0200

    Modify fields in Park subclass to be accept null value

--- HikingBlog/HikingBlog/Park.cs
+++ HikingBlog/HikingBlog/Park.cs
@@ -6,9 +6,9 @@ namespace HikingBlog
 {
     public class Park: Destination
     {
-        private bool HasPlayground;
-        private bool IsDogFriendlyl;
-        public Park(string name, string creator, string description, string imageUrl, string region, bool hasPlayground, bool isDogFriendlyl) : base(name, creator, description, imageUrl, region)
+        private bool? HasPlayground;
+        private bool? IsDogFriendlyl;
+        public Park(string name, string creator, string description, string imageUrl, string region, bool? hasPlayground, bool? isDogFriendlyl) : base(name, creator, description, imageUrl, region)
         {
             HasPlayground = hasPlayground;
             IsDogFriendlyl = isDogFriendlyl;
