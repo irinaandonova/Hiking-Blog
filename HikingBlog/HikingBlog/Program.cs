12e8496d4db3d2c3ae4baa57baafc68e03dea4b3
irinaandonova <irinaandonova97@gmail.com>
2022-11-14 13:11:41 +0200

    Add some more test data

--- HikingBlog/HikingBlog/Program.cs
+++ HikingBlog/HikingBlog/Program.cs
@@ -1,5 +1,6 @@
 ﻿using HikingBlog.Extensions;
 using HikingBlog.Models;
+using HikingBlog.Services;
 using System;
 
 namespace HikingBlog
@@ -8,16 +9,23 @@ namespace HikingBlog
     {
         static void Main(string[] args)
         {
+            WebAPI webApi = new WebAPI();
+
             User user = new User("Ira", "ira@", 2);
-            Park park = new Park("Layta", user, "Amazing park", "image.com", "Plovdiv", null, null);
+            Park park = new Park("Layta", user, "Amazing park", "image.com", "Plovdiv", true, false);
             park.CreateComment(user, "Beautiful Park");
             park.ShowComments();
+            webApi.AddDestination(park);
 
             Seaside seaside = new Seaside("Konstantin i Elena", user, "Amazing beach", "image1.com", "Varna", true, true);
             seaside.AddUmbrellaPrices(5.00);
             seaside.ShowUmbrellaPrices();
-            HikingTrail hikingTrail = new HikingTrail("hsh", user, "hsh", "sjsj", "djsj", 1, 20);
+            webApi.AddDestination(seaside);
+
+            HikingTrail hikingTrail = new HikingTrail("Rilski ezera", user, "Krasiva puteka", "sjsj.com", "Rila", 2, 20);
             hikingTrail.RateDestination(1, user);
+            webApi.AddDestination(hikingTrail);
+            webApi.GetAllDestinations();
         }
     }
 }
