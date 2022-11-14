8d3d4ec21dd6d82fd1aca2673f5dd8a5abbf61ed
irinaandonova <irinaandonova97@gmail.com>
2022-11-14 13:07:13 +0200

    Add custom exception

---
+++ HikingBlog/HikingBlog/CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HikingBlog
+{
+    internal class CustomException: Exception
+    {
+        public CustomException(string msg) : base(msg)
+        { }
+        public static string InvalidEmail(string email)
+        {
+            if (!email.Contains('@'))
+                throw new Exception("Invalid email");
+            else
+                return email;               
+        }
+
+    }
+}
--- HikingBlog/HikingBlog/Models/User/User.cs
+++ HikingBlog/HikingBlog/Models/User/User.cs
@@ -8,17 +8,25 @@ namespace HikingBlog.Models
     {
         public User(string username, string email, int hikingLevel)
         {
-            Username = username;
-            if (!email.Contains("@")) throw new Exception("Invalid email");
-            Email = email;
-            if (hikingLevel == 1 || hikingLevel == 2 || hikingLevel == 3)
+            try
             {
-                HikingLevel = hikingLevel;
+                Username = username;
+                Email = CustomException.InvalidEmail(email);
+                Email = email;
+                if (hikingLevel == 1 || hikingLevel == 2 || hikingLevel == 3)
+                {
+                    HikingLevel = hikingLevel;
+                }
+                else
+                {
+                    throw new Exception("Invalid hiking level");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                throw new Exception("Invalid hiking level");
+                Console.WriteLine(ex.Message);
             }
+            
         }
         public string Username { get; set; }
         public string Email { get; set; }
