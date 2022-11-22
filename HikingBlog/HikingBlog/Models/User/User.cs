c3873784f97d1786391d7e667613bcc655d2c1a5
irinaandonova <irinaandonova97@gmail.com>
2022-11-22 12:01:52 +0200

    Add  new property to User class

--- HikingBlog/HikingBlog/Models/User/User.cs
+++ HikingBlog/HikingBlog/Models/User/User.cs
@@ -6,12 +6,17 @@ namespace HikingBlog.Models
 {
     public class User
     {
-        public User(string username, string email)
-        {                         
-                Email = email;
-                Username = username;
+        public User(string username, string email, int hikingSkill)
+        {
+            Email = email;
+            Username = username;
+            HikingSkill = hikingSkill;
         }
+
         public string Username { get; set; }
+
         public string Email { get; set; }
+
+        public int HikingSkill { get; set; }
     }
 }
