93c7ef429be50fec5069c6d17bcad9631d650c90
irinaandonova <irinaandonova97@gmail.com>
2022-11-22 13:49:38 +0200

    Separate services

--- HikingBlog/HikingBlog/Services/CommentService.cs
+++ HikingBlog/HikingBlog/Services/CommentService.cs
@@ -10,6 +10,56 @@ namespace NatureBlog.Services
 {
     internal class CommentService
     {
-       
+
+        public void CreateComment(Destination destination, User creator, string text)
+        {
+            Comment comment = new Comment(creator, text);
+            string id = comment.Id;
+            destination.AddComment(id, comment);
+        }
+
+        public void DeleteComment(Destination destination, User creator, string id)
+        {
+            try
+            {
+                if (!destination.CompareUser(id, creator))
+                    throw new InvalidOperationException("Current user isn't the creator of the comment!");
+                destination.RemoveComment(id);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("No comment found with given id!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Insert all values!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void EditComment(Destination destination, User creator, string id, string text)
+        {
+            try
+            {
+                if (!destination.CompareUser(id, creator))
+                    throw new UserNotCreatorException("Current user isn't the creator of the comment!");
+                destination.EditComment(id, text);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine($"Insert all values!");
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("No comment found with given id!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
