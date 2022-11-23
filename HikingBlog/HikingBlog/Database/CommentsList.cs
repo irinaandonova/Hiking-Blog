bae8a5b7c2c6b6292b0593f3a0018a2593b69892
irinaandonova <irinaandonova97@gmail.com>
2022-11-23 15:36:27 +0200

    Simulate database

---
+++ HikingBlog/HikingBlog/Database/CommentsList.cs
@@ -0,0 +1,40 @@
+﻿using NatureBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Database
+{
+    internal class CommentsList
+    {
+        private CommentsList()
+        { }
+
+        private static CommentsList _instance;
+
+        private static readonly object _lock = new object();
+
+        public static CommentsList GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new CommentsList();
+                        _instance.AllComments = new List<Destination> { };
+                    }
+                }
+            }
+            return _instance;
+        }
+        public void Attach(ISubscriber subscriber)
+        {
+
+        }
+        public List<Destination> AllComments;
+    }
+}
---
+++ HikingBlog/HikingBlog/Database/UserList.cs
@@ -0,0 +1,37 @@
+﻿using NatureBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Database
+{
+    public class UserList
+    {
+        private UserList()
+        { }
+
+        private static UserList _instance;
+
+        private static readonly object _lock = new object();
+
+        public static UserList GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new UserList();
+                        _instance.AllUsers = new List<User> { };
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public List<User> AllUsers;
+    }
+}
