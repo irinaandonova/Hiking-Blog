f7d5a23735de78df14c44e3d70a2191c5fac86e2
irinaandonova <irinaandonova97@gmail.com>
2022-11-23 12:01:52 +0200

    Add replies

---
+++ HikingBlog/HikingBlog/Models/Comment/Reply.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Models
+{
+    internal class Reply : Comment
+    {
+        public Reply(User creator, string text, string mainCommentId): base(creator, text)
+        {
+            mainCommentId = MainCommentId;
+        }
+
+        public string MainCommentId { get; }
+    }
+}
