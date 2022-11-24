63ef4c8c25d783c95de4c791ea2f2aef2c7f105a
irinaandonova <irinaandonova97@gmail.com>
2022-11-24 13:03:43 +0200

    Create destination service interface

---
+++ HikingBlog/HikingBlog/Services/DestinationServices/Interfaces/IDestinationService.cs
@@ -0,0 +1,17 @@
+﻿using NatureBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Services.DestinationServices.Interfaces
+{
+    internal interface IDestinationService
+    {
+            bool Add(Destination destination);
+            bool Delete(Destination destination);
+            bool Update(Guid destinationId, Destination updatedDestination);
+            ICollection<Destination> GetMostVisited(int numberOfDestinations);
+    }
+}
