8f99128e85245242ba5914523c5424f602d3ca22
irinaandonova <irinaandonova97@gmail.com>
2022-11-25 12:18:05 +0200

    Create user interface

---
+++ HikingBlog/HikingBlog/Models/User/Interfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Models
+{
+    internal interface IUser
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public int HikingSkill { get; set; }
+
+        //public bool IsSubscribed { get; set; } = false;
+    }
+}
