5a557794e34e7756a86c329abe9accbd6dbeaf5a
irinaandonova <irinaandonova97@gmail.com>
2022-11-25 11:03:56 +0200

    Add methods to interface

--- HikingBlog/HikingBlog/Services/DestinationServices/Interfaces/IDestinationService.cs
+++ HikingBlog/HikingBlog/Services/DestinationServices/Interfaces/IDestinationService.cs
@@ -9,9 +9,38 @@ namespace NatureBlog.Services.DestinationServices.Interfaces
 {
     internal interface IDestinationService
     {
-            bool Add(Destination destination);
-            bool Delete(Destination destination);
-            bool Update(Guid destinationId, Destination updatedDestination);
-            ICollection<Destination> GetMostVisited(int numberOfDestinations);
+        bool Add(Destination destination);
+
+        bool Delete(Destination destination);
+
+        bool Update(Guid destinationId, Destination updatedDestination);
+
+        List<Destination> GetMostVisited(int numberOfDestinations);
+
+        Destination GetDestination(Guid destinationId);
+
+        List<Seaside> GetSeasides();
+
+        List<HikingTrail> GetHikingTrails();
+
+        List<Park> GetParks();
+
+        List<HikingTrail> FilterHikingTrails(int difficulty);
+
+        List<Seaside> FilterSeaside(bool isGuarded);
+
+        List<Park> FilterPArk(bool isDogFriendly, bool hasPlayground);
+
+        List<Destination> FikterByRegeon(string regeon);
+
+        List<Destination> SearchDestination(string searchWord);
+
+        List<Destination> SortDestinations(string condition);
+
+        void AddUmbrellaPrices(Guid id, double umbrellaPrice);
+
+        void RateDestination(Guid destinationId, int ratingValue, User user);
+
+        void ChangeDifficulty(Guid destinationId, int difficulty);
     }
 }
