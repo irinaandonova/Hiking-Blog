6505769f4c35f625c7659cf1d5a4c6632a40b572
irinaandonova <irinaandonova97@gmail.com>
2022-11-29 20:55:28 +0200

    Add interfaces to destination

---
+++ HikingBlog/Application/Destinations/Interfaces/IDestinationService.cs
@@ -0,0 +1,35 @@
+﻿using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.Destinations.Interfaces
+{
+    internal interface IDestinationService
+    {
+        List<Destination> GetMostVisited(int numberOfDestinations);
+
+        Destination GetDestination(Guid destinationId);
+
+        List<Seaside> GetSeasides();
+
+        List<HikingTrail> GetHikingTrails();
+
+        List<Park> GetParks();
+
+        List<HikingTrail> FilterHikingTrails(int difficulty);
+
+        List<Seaside> FilterSeaside(bool isGuarded);
+
+        List<Park> FilterPArk(bool isDogFriendly, bool hasPlayground);
+
+        List<Destination> FikterByRegeon(string regeon);
+
+        List<Destination> SearchDestination(string searchWord);
+
+        List<Destination> SortDestinations(string condition);
+
+        bool AddUmbrellaPrices(Guid id, double umbrellaPrice);
+
+        bool RateDestination(Guid destinationId, int ratingValue, User user);
+        
+        bool ChangeDifficulty(Guid destinationId, int difficulty);
+    }
+}
