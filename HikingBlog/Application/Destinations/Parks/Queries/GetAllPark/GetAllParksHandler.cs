b8ac9cada83ef37577139cfa1c477249becd07bb
irinaandonova <irinaandonova97@gmail.com>
2022-11-30 17:13:58 +0200

    Add get all parks query

---
+++ HikingBlog/Application/Destinations/Parks/Queries/GetAllPark/GetAllParksHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using NatureBlog.Application.Destinations.Interfaces;
+using NatureBlog.Application.Destinations.Parks.Queries.GetAllPark;
+using NatureBlog.Application.Exceptions;
+using NatureBlog.Domain.Models;
+
+namespace Application.Destinations.Parks.Queries.GetAllPark
+{
+    public class GetAllParksHandler : IRequestHandler<GetAllParksCommand, List<Park>>
+    {
+        private readonly IDestinationRepository _repository;
+
+        public GetAllParksHandler(IDestinationRepository DestinationRepository)
+        {
+            _repository = DestinationRepository;
+        }
+
+        public Task<List<Park>> Handle(GetAllParksCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                List<Park> allParks = _repository.GetAllParks();
+
+                if (allParks.Count() < 0)
+                    throw new DestinationNotFoundException("There are no elements in the collection");
+
+                return Task.FromResult(allParks);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in GetAllParks Method:" + ex.Message);
+                return null;
+            }
+        }
+    }
+}
