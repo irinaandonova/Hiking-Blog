fbb8df989e718175f91b2a11c67f76333a24a897
irinaandonova <irinaandonova97@gmail.com>
2022-11-30 17:12:15 +0200

    Add create and update commands for hiking trails

---
+++ HikingBlog/Application/Destinations/HikingTrails/Commands/CreateHikingTrail/CreateHikingTrailCommand.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using NatureBlog.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Application.Destinations.HikingTrails.Commands.CreateHikingTrail
+{
+    internal class CreateHikingTrailCommand: IRequest<bool>
+    {
+        public string Name { get; }
+
+        public User Creator { get; }
+
+        public string Description { get; }
+
+        public string ImageUrl { get; }
+
+        public string Region { get; }
+
+        public bool IsGuarded { get; set; }
+
+        public int Difficulty { get; set; }
+
+        public int Duration { get; set; }
+
+        public int? RatingScore { get; set; } = null;
+
+        public Dictionary<Guid, Comment> Comments { get; set; } = new Dictionary<Guid, Comment> { };
+
+        public Dictionary<User, int> Ratings { get; set; } = new Dictionary<User, int> { };
+
+        public List<User> Visitors { get; set; } = new List<User> { };
+    }
+}
---
+++ HikingBlog/Application/Destinations/HikingTrails/Commands/UpdateHikingTrail/UpdateHikingTrailCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using NatureBlog.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Destinations.HikingTrails.Commands.UpdateHikingTrail
+{
+    internal class UpdateHikingTrailCommand: IRequest<bool>
+    {
+        public readonly Guid Id;
+
+        public HikingTrail hikingTrail;
+
+        public User user;
+    }
+}
