8ea1eef4dc4d29347719f34a935888a6d0d1f1d1
irinaandonova <irinaandonova97@gmail.com>
2022-11-30 11:27:07 +0200

    Use try/catch block in  application layer rather than in infrastructure

--- HikingBlog/Application/Destinations/Seaside/Commands/CreateSeaside/CreateSeasideHandler.cs
+++ HikingBlog/Application/Destinations/Seaside/Commands/CreateSeaside/CreateSeasideHandler.cs
@@ -1,14 +1,10 @@
-﻿using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 using NatureBlog.Application.Destinations.Interfaces;
-using NatureBlog.Application.Destinations.Seasides.Commands.CreateDestination;
 using NatureBlog.Domain.Models;
 
 namespace NatureBlog.Application.Destinations.Seasides.Commands.CreateDestination
 {
-    public class CreateSeasideHandler : IRequestHandler<CreateSeasideCommand, Guid>
+    public class CreateSeasideHandler : IRequestHandler<CreateSeasideCommand, bool>
     {
         private readonly IDestinationRepository _DestinationRepository;
 
@@ -17,11 +13,18 @@ namespace NatureBlog.Application.Destinations.Seasides.Commands.CreateDestinatio
             _DestinationRepository = DestinationRepository;
         }
 
-        public Task<Guid> Handle(CreateSeasideCommand command, CancellationToken cancellationToken)
+        public Task<bool> Handle(CreateSeasideCommand command, CancellationToken cancellationToken)
         {
-            Seaside seaside = new Seaside(command.Name, command.Creator, command.Description, command.ImageUrl, command.Region, command.IsGuarded, command.OffersUmbrella);
-
-            return Task.FromResult(seaside.Id); 
+            try
+            {
+                Seaside seaside = new Seaside(command.Name, command.Creator, command.Description, command.ImageUrl, command.Region, command.IsGuarded, command.OffersUmbrella);
+                return Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in Add Method:" + ex.Message);
+                return Task.FromResult(false);
+            }
         }
     }
 }
