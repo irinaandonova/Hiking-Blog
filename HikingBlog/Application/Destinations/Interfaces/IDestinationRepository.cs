030b9fea0a78bdfe70ee7243945e6d2f401501e1
irinaandonova <irinaandonova97@gmail.com>
2022-11-30 11:07:29 +0200

    Delete useless interfaces

--- HikingBlog/Application/Destinations/Interfaces/IDestinationRepository.cs
+++ HikingBlog/Application/Destinations/Interfaces/IDestinationRepository.cs
@@ -4,25 +4,23 @@ namespace NatureBlog.Application.Destinations.Interfaces
 {
     public interface IDestinationRepository
     {
-        Dictionary<Guid, Destination> AllDestinations { get; }
-
-        List<Destination> GetMostVisited(int numberOfDestinations);
+        List<Destination> GetMostVisited();
 
         Destination GetDestination(Guid destinationId);
 
-        List<Seaside> GetSeasides();
+        List<Seaside> GetAllSeasides();
 
-        List<HikingTrail> GetHikingTrails();
+        List<HikingTrail> GetAllHikingTrails();
 
-        List<Park> GetParks();
+        List<Park> GetAllParks();
 
         List<HikingTrail> FilterHikingTrails(int difficulty);
 
         List<Seaside> FilterSeaside(bool isGuarded);
 
-        List<Park> FilterPArk(bool isDogFriendly, bool hasPlayground);
+        List<Park> FilterPark(bool isDogFriendly, bool hasPlayground);
 
-        List<Destination> FikterByRegeon(string regeon);
+        List<Destination> FilterByRegion(string regeon);
 
         List<Destination> SearchDestination(string searchWord);
 
