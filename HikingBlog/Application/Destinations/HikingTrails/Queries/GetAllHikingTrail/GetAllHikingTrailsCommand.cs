1d3245110cb37ec599b5c243f9e25c21dc7bdb87
irinaandonova <irinaandonova97@gmail.com>
2022-11-30 17:12:41 +0200

    Add get all hiking trails query

---
+++ HikingBlog/Application/Destinations/HikingTrails/Queries/GetAllHikingTrail/GetAllHikingTrailsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using NatureBlog.Domain.Models;
+
+namespace Application.Destinations.HikingTrails.Queries.GetAllHikingTrail
+{
+    internal class GetAllHikingTrailsCommand: IRequest<List<HikingTrail>>
+    {
+    }
+}
---
+++ HikingBlog/Application/Destinations/HikingTrails/Queries/GetAllHikingTrail/GetAllHikingTrailsHandler.cs
@@ -0,0 +1,41 @@
+﻿using NatureBlog.Application.Destinations.Interfaces;
+using NatureBlog.Application.Destinations.Seasides.Queries.GetAllSeaside;
+using NatureBlog.Application.Exceptions;
+using NatureBlog.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Destinations.HikingTrails.Queries.GetAllHikingTrail
+{
+    internal class GetAllHikingTrailsHandler
+    {
+        private readonly IDestinationRepository _repository;
+
+        public GetAllSeasidesHandler(IDestinationRepository DestinationRepository)
+        {
+            _repository = DestinationRepository;
+        }
+
+        public Task<List<HikingTrail>> Handle(GetAllHikingTrailsCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                List<HikingTrail> allHikingTrails = _repository.GetAllHikingTrails();
+
+                if (allHikingTrails.Count() < 0)
+                    throw new DestinationNotFoundException("There are no elements in the collection");
+
+                return Task.FromResult(allHikingTrails);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in GetHikingTrails Method:" + ex.Message);
+                return null;
+            }
+        }
+    }
+}
