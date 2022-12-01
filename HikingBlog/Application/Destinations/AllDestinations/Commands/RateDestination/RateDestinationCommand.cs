4ae3894be04985ecb68111e0da80f0a1d477a9fc
irinaandonova <irinaandonova97@gmail.com>
2022-12-01 12:01:23 +0200

    Add rate destination command

---
+++ HikingBlog/Application/Destinations/AllDestinations/Commands/RateDestination/RateDestinationCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace NatureBlog.Application.Destinations.AllDestinations.Commands.RateDestination
+{
+    public class RateDestinationCommand : IRequest<bool>
+    {
+        public readonly Guid destinationId;
+
+        public int ratingValue;
+
+        public readonly Guid userId;
+    }
+}
