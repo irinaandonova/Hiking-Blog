830c3ff2c1db8aa999843846e5c2a12d83c44572
irinaandonova <irinaandonova97@gmail.com>
2022-12-01 14:41:59 +0200

    Add lost changes

---
+++ HikingBlog/Application/Comments/Interfaces/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using NatureBlog.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Application.Comments.Interfaces
+{
+    internal interface ICommentService
+    {
+        void CreateComment(Guid destinationId, User creator, string text, string mainCommentId);
+
+        void DeleteComment(Guid destinationId, User creator, Guid commentId);
+
+        void EditComment(Guid destinationId, User creator, Guid commentId, string text);
+    }
+}
