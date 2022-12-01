1d2b899692910ab4788097dc563af57e6123106b
irinaandonova <irinaandonova97@gmail.com>
2022-12-01 12:02:33 +0200

    Add umbrella prices command

---
+++ HikingBlog/Application/Destinations/Seasides/Commands/AddUmbrellaPrices/AddUmbrellaPricesCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace NatureBlog.Application.Destinations.Seasides.Commands.AddUmbrellaPrices
+{
+    public class AddUmbrellaPricesCommand : IRequest<bool>
+    {
+        public Guid seasideId;
+
+        public double umbrellaPrice;
+    }
+}
