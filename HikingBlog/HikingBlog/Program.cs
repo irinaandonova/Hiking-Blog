b9cc3a81b9ab28b8cfc35e028403b940e2ef16a7
irinaandonova <irinaandonova97@gmail.com>
2022-12-02 14:19:44 +0200

    Delete cache

--- HikingBlog/HikingBlog/Program.cs
+++ HikingBlog/HikingBlog/Program.cs
@@ -3,38 +3,37 @@ using Microsoft.Extensions.DependencyInjection;
 using NatureBlog.Application.Destinations.Interfaces;
 using NatureBlog.Application.Destinations.Seasides.Commands.CreateDestination;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web;
- static async Task MainFunc(string[] args)
+using NatureBlog.Application.Destinations.AllDestinations.Queries.GetMostVisited;
+using NatureBlog.Domain.Models;
+using System.Collections.Generic;
+using Castle.Core.Logging;
+using Application;
+using System.Reflection;
+
+static async Task MainFunc()
 {
     var serviceCollection = new ServiceCollection()
-        .AddMediatR(typeof(IDestinationRepository))
+        .AddMediatR(Assembly.GetExecutingAssembly())
         .BuildServiceProvider();
 
     var mediator = serviceCollection.GetRequiredService<IMediator>();
-
-    await mediator.Send(new CreateSeasideCommand
+    
+    bool result = await mediator.Send(new CreateSeasideCommand
     {
         Name = "Konstantin and Elena Beach",
-
-    })
-
-         var orderId = await mediator.Send(new CreateOrderCommand
-         {
-             BuyerName = "TheBuyer",
-             OrderItems = new List<OrderItemDto>
-                {
-                    new() { Quantity = 1, Price = 5, ProductName = "Telefon" },
-                    new() { Quantity = 2, Price = 7, ProductName = "TV" }
-                }
-         });
-
-    Console.WriteLine($"Order created with {orderId}");
-
-
-    var orders = await mediator.Send(new GetOrdersListQuery());
-
-    var productId = await mediator.Send(new CreateProductCommand { Name = "test", Price = 10 });
-    var products = await mediator.Send(new GetProductsListQuery());
+        CreatorId = Guid.NewGuid(),
+        Description = "Great beach",
+        ImageUrl = "img.jpg",
+        Region = "Varna",
+        IsGuarded = true,
+        OffersUmbrella = true
+    });
+    
+    var destinations = await mediator.Send(new GetMostVisitedQuery());
+    foreach(var destination in destinations)
+        Console.WriteLine(destination.Name);
 }
+
+MainFunc();
+
