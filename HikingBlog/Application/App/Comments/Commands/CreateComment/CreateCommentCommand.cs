5f62f13e72151c02f10c99ae408c30a606b8aa02
irinaandonova <irinaandonova97@gmail.com>
2022-12-04 21:06:22 +0200

    Fix accessibility and make fields properties

---
+++ HikingBlog/Application/App/Comments/Commands/CreateComment/CreateCommentCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Application.App.Comments.Commands.CreateComment
+{
+    public class CreateCommentCommand : IRequest<bool>
+    {
+        public Guid destinationId;
+
+        public string text;
+
+        public Guid creatorId;
+    }
+}
