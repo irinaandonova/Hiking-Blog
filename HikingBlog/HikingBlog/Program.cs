d1da174c0d55d924afa200ff4967cad13b5a3aef
irinaandonova <irinaandonova97@gmail.com>
2022-12-04 20:45:04 +0200

    Add scope

--- HikingBlog/HikingBlog/Program.cs
+++ HikingBlog/HikingBlog/Program.cs
@@ -1,39 +1,33 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using NatureBlog.Application.Destinations.AllDestinations.Queries.GetMostVisited;
 using NatureBlog.Application.Destinations.Interfaces;
 using NatureBlog.Application.Destinations.Seasides.Commands.CreateDestination;
 using System;
-using System.Threading.Tasks;
-using NatureBlog.Application.Destinations.AllDestinations.Queries.GetMostVisited;
-using NatureBlog.Domain.Models;
-using System.Collections.Generic;
-using Castle.Core.Logging;
-using Application;
 using System.Reflection;
+using System.Threading.Tasks;
+using NatureBlog.Infrastructure.Repositories;
 
-static async Task MainFunc()
+var serviceCollection = new ServiceCollection()
+    .AddMediatR(Assembly.GetExecutingAssembly())
+    .AddScoped(typeof(IDestinationRepository), typeof(DestinationRepository))
+    .BuildServiceProvider();
+    
+
+var mediator = serviceCollection.GetRequiredService<IMediator>();
+
+bool result = await mediator.Send(new CreateSeasideCommand
 {
-    var serviceCollection = new ServiceCollection()
-        .AddMediatR(Assembly.GetExecutingAssembly())
-        .BuildServiceProvider();
+    Name = "Konstantin and Elena Beach",
+    CreatorId = Guid.NewGuid(),
+    Description = "Great beach",
+    ImageUrl = "img.jpg",
+    Region = "Varna",
+    IsGuarded = true,
+    OffersUmbrella = true
+});
 
-    var mediator = serviceCollection.GetRequiredService<IMediator>();
-    
-    bool result = await mediator.Send(new CreateSeasideCommand
-    {
-        Name = "Konstantin and Elena Beach",
-        CreatorId = Guid.NewGuid(),
-        Description = "Great beach",
-        ImageUrl = "img.jpg",
-        Region = "Varna",
-        IsGuarded = true,
-        OffersUmbrella = true
-    });
-    
-    var destinations = await mediator.Send(new GetMostVisitedQuery());
-    foreach(var destination in destinations)
-        Console.WriteLine(destination.Name);
-}
-
-MainFunc();
+var destinations = await mediator.Send(new GetMostVisitedQuery());
+foreach (var destination in destinations)
+    Console.WriteLine(destination.Name);
 
