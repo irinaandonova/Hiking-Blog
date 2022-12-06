a1f96a2f7068ea5e945ca47853b4603d394f86e3
irinaandonova <irinaandonova97@gmail.com>
2022-12-06 11:27:04 +0200

    Add new model

---
+++ HikingBlog/Domain/Models/Region/Region.cs
@@ -0,0 +1,15 @@
+﻿using NatureBlog.Domain.Models;
+
+namespace NatureBLog.Domain.Models.Region
+{
+    public class Region
+    {
+        public Guid Id;
+
+        public string Name { get; set; }
+
+        public string Cordinates { get; set; }
+
+        public ICollection<Destination> Destinations { get; set; }
+    }
+}
