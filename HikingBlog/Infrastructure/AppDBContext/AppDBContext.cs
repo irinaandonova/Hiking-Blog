95caaa305836d4c232f925b1883b8f6478a31b64
irinaandonova <irinaandonova97@gmail.com>
2022-12-06 11:27:21 +0200

    Add DB context

---
+++ HikingBlog/Infrastructure/AppDBContext/AppDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Infrastructure.AppDBContext
+{
+    public class AppDBContext : DbContext
+    {
+        private const string ConnectionString = @"Server=DESKTOP-UMPKKH0\SQLEXPRESS;Database=Nature Blog;Trusted_Connection=True;";
+
+        public DbSet<Destination> Destinations => Set<Destination>();
+
+        // DbSet<Comment> Comments => Set<Comment>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Destination>()
+                .HasKey(d => d.Id);
+
+            modelBuilder.Entity<Comment>()
+                .HasKey(c => c.Id);
+                
+        }
+        */
+    }
+}
