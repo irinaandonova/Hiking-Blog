67b6e9edc03b64956b498644d65db3d6b0ef7a32
irinaandonova <irinaandonova97@gmail.com>
2022-12-06 12:13:43 +0200

    Add configuration

---
+++ HikingBlog/Infrastructure/EntityConfiguration/RatingConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NatureBlog.Domain.Models.Rating;
+
+namespace NatureBlog.Infrastructure.EntityConfiguration
+{
+    public class RatingConfiguration: IEntityTypeConfiguration<Rating>
+    {
+        public void Configure(EntityTypeBuilder<Rating> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(x => x.User);
+
+            entityTypeBuilder.Property(x => x.User)
+                .IsRequired();
+
+            entityTypeBuilder.Property(x => x.RatingValue)
+                .IsRequired();
+
+        }
+    }
+}
