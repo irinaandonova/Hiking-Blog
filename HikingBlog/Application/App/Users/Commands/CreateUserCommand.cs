d787cedef80019621825c049922fc743902f92a8
irinaandonova <irinaandonova97@gmail.com>
2022-12-08 17:58:52 +0200

    Add create user command

---
+++ HikingBlog/Application/App/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Application.App.Users
+{
+    public class CreateUserCommand : IRequest<bool>
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public int HikingSkill { get; set; }
+    }
+}
---
+++ HikingBlog/Application/App/Users/Commands/CreateUserHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using NatureBlog.Application.Exceptions;
+using NatureBlog.Application.Repositories;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.App.Users
+{
+    public class CreateUserHandler : IRequestHandler<CreateUserCommand, bool>
+    {
+        private readonly IUserRepository _userRepository;
+        public CreateUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<bool> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(command.Username) || String.IsNullOrEmpty(command.Email))
+                    throw new AllFieldsMustBeFilled("Username or email isn't filled!");
+                if (command.HikingSkill < 1 || command.HikingSkill > 3)
+                    throw new OutOfRangeException("Hiking level must be between 1 and 3!");
+
+                User user = new User { Id = Guid.NewGuid(), Username = command.Username, Email = command.Email, HikingSkill = command.HikingSkill};
+                _userRepository.Add(user);
+
+                return Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Excetion in the Add User method: ", ex.Message);
+                return Task.FromResult(false);
+            }
+        }
+    }
+        
+}
+
+
