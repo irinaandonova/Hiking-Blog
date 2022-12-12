0fe86b0612532ddadbff79e5b32b34504fcd9475
irinaandonova <irinaandonova97@gmail.com>
2022-12-12 11:04:51 +0200

    Add unit of work interface

---
+++ HikingBlog/Application/Repositories/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace NatureBlog.Application.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public ICommentRepository CommentRepository { get; }
+
+        public IDestinationRepository DestinationRepository { get; }
+
+        public IRegionRepository RegionRepository { get; }
+
+        public IUserRepository UserRepository { get; }
+
+        Task Save();
+    }
+}
