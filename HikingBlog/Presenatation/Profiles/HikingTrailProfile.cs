9ccdf3ce3591fe1ffa2bf1269d32aa67b2e08e6b
irinaandonova <irinaandonova97@gmail.com>
2022-12-15 23:28:42 +0200

    Map profiles

---
+++ HikingBlog/Presenatation/Profiles/HikingTrailProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NatureBlog.Domain.Models;
+using NatureBlog.Presenatation.Dto.Destination;
+
+namespace NatureBlog.Presenatation.Profiles
+{
+    public class HikingTrailProfile : Profile
+    {
+        public HikingTrailProfile()
+        {
+            CreateMap<HikingTrailPostDto, HikingTrail>();
+            CreateMap<HikingTrail, HikingTrailGetDto>();
+        }
+    }
+}
---
+++ HikingBlog/Presenatation/Profiles/ParkProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NatureBlog.Domain.Models;
+using Presenatation.Dto.Destination.Park;
+
+namespace NatureBlog.Presenatation.Profiles
+{
+    public class ParkProfile : Profile
+    {
+        public ParkProfile()
+        {
+            CreateMap<ParkPostDto, Park>();
+            CreateMap<Park, ParkGetDto>();
+        }
+    }
+}
