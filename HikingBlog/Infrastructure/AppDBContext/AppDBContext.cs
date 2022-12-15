f1065c2e6e1fa5f04c73ba0b35ec4e89415d70ef
irinaandonova <irinaandonova97@gmail.com>
2022-12-15 15:58:07 +0200

    Remove moved files

--- HikingBlog/Infrastructure/AppDBContext/AppDBContext.cs
+++ HikingBlog/Infrastructure/AppDBContext/AppDBContext.cs
@@ -6,8 +6,11 @@ namespace NatureBlog.Infrastructure
 {
     public class AppDBContext : DbContext
     {
-        private const string ConnectionString = @"Server=DESKTOP-UMPKKH0\SQLEXPRESS;Database=NatureBlog;Encrypt=False;Trusted_Connection=True;";
 
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
         public DbSet<Destination> Destinations => Set<Destination>();
 
         public DbSet<Comment> Comments => Set<Comment>();
@@ -24,12 +27,7 @@ namespace NatureBlog.Infrastructure
 
         //public DbSet<UserVisitedDestinations> UserVisitedDestinations => Set<UserVisitedDestinations>();
         
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionString);
-            optionsBuilder.EnableDetailedErrors();
-            base.OnConfiguring(optionsBuilder);
-        }
+       
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
