9db09801c881a52f900184e63421a7a7c221615d
irinaandonova <irinaandonova97@gmail.com>
2022-12-19 10:20:48 +0200

    Add method

--- NatureBlog/Infrastructure/Repositories/RegionRepository.cs
+++ NatureBlog/Infrastructure/Repositories/RegionRepository.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NatureBlog.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace NatureBlog.Infrastructure.Repositories
 {
@@ -16,10 +17,17 @@ namespace NatureBlog.Infrastructure.Repositories
         {
             _dbContext = dbContext;
         }
+
         public async Task Add(Region region)
         {
              await _dbContext.Regions.AddAsync(region);
             _dbContext.SaveChanges();   
+        }
+
+        public async Task<List<Region>> GetAll()
+        {
+            List<Region> result = await _dbContext.Regions.ToListAsync();
+            return result;
         }
     }
 }
