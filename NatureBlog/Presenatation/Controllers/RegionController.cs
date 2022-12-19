32979ae4940e0d04df01c7c786df9e87857b9612
irinaandonova <irinaandonova97@gmail.com>
2022-12-19 09:17:32 +0200

    Add controller

--- NatureBlog/Presenatation/Controllers/RegionController.cs
+++ NatureBlog/Presenatation/Controllers/RegionController.cs
@@ -10,43 +10,41 @@ namespace Presenatation.Controllers
     [ApiController]
     public class RegionController : ControllerBase
     {
-        public class RegionController : ControllerBase
+        public readonly IMapper _mapper;
+        public readonly IMediator _mediator;
+
+        public RegionController(IMapper mapper, IMediator mediator)
         {
-            public readonly IMapper _mapper;
-            public readonly IMediator _mediator;
-
-            public RegionController(IMapper mapper, IMediator mediator)
-            {
-                _mediator = mediator;
-                _mapper = mapper;
-            }
-
-            // GET: api/<RegionController>
-            [HttpGet]
-            public async Task<IActionResult> GetAll()
-            {
-                var result = await _mediator.Send(new GetAllRegionsCommand());
-                var mappedResult = _mapper.Map<List<RegionGetDto>>(result);
-                return Ok(mappedResult);
-            }
-
-            // POST api/<RegionController>
-            [HttpPost]
-            public async Task<IActionResult> Post([FromBody] RegionPostDto region)
-            {
-                if (!ModelState.IsValid)
-                    return BadRequest(ModelState);
-
-                var result = await _mediator.Send(new CreateRegionCommand
-                {
-                    Name = region.Name,
-                    Cordinates = region.Cordinates
-                });
-                if (result is null)
-                    return BadRequest();
-
-                var mappedResult = _mapper.Map<RegionGetDto>(result);
-                return Ok(mappedResult);
-            }
+            _mediator = mediator;
+            _mapper = mapper;
         }
+
+        // GET: api/<RegionController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllRegionsCommand());
+            var mappedResult = _mapper.Map<List<RegionGetDto>>(result);
+            return Ok(mappedResult);
+        }
+
+        // POST api/<RegionController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] RegionPostDto region)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _mediator.Send(new CreateRegionCommand
+            {
+                Name = region.Name,
+                Cordinates = region.Cordinates
+            });
+            if (result is null)
+                return BadRequest();
+
+            var mappedResult = _mapper.Map<RegionGetDto>(result);
+            return Ok(mappedResult);
+        }
+    }
 }
