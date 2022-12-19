9dc54e59c036ddfe452318688012022b3df1fdc3
irinaandonova <irinaandonova97@gmail.com>
2022-12-19 09:23:37 +0200

    Add request

---
+++ NatureBlog/Application/App/Regions/GetAllRegions/GetAllRegionsCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using NatureBlog.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Application.App.Regions.GetAllRegions
+{
+    public class GetAllRegionsCommand : IRequest<List<Region>>
+    {
+    }
+}
---
+++ NatureBlog/Application/App/Regions/GetAllRegions/GetAllRegionsHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using NatureBlog.Application.Repositories;
+using NatureBlog.Domain.Models;
+using NatuteBlog.Application.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Application.App.Regions.GetAllRegions
+{
+    public class GetAllRegionsHandler : IRequestHandler<GetAllRegionsCommand, List<Region>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllRegionsHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<Region>> Handle(GetAllRegionsCommand command, CancellationToken cancellationToken)
+        {
+
+            return await _unitOfWork.RegionRepository.GetAll();
+        }
+    }
+}
