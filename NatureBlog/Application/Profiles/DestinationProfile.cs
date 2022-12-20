4cccb35f51d608925074304eeb62b8a848124322
irinaandonova <irinaandonova97@gmail.com>
2022-12-20 10:24:39 +0200

    Add destination profile

---
+++ NatureBlog/Application/Profiles/DestinationProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using NatureBlog.Application.Dto.Destination.Destination;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.Profiles
+{
+    public class DestinationProfile : Profile
+    {
+        public DestinationProfile()
+        {
+            CreateMap<HikingTrail, DestinationGetDto>();
+            CreateMap<Seaside, DestinationGetDto>();
+            CreateMap<Park, DestinationGetDto>();
+        }
+    }
+}
