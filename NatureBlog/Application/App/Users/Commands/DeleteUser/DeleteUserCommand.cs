fd0df0b15830a7e6a476df0ac36c6715d0494db5
irinaandonova <irinaandonova97@gmail.com>
2022-12-20 17:02:34 +0200

    Add delete user command

---
+++ NatureBlog/Application/App/Users/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace NatureBlog.Application.App.Users.Commands.DeleteUser
+{
+    public class DeleteUserCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ NatureBlog/Application/App/Users/Commands/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using NatureBlog.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Application.App.Users.Commands.DeleteUser
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteUserHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(DeleteUserCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                _unitOfWork.UserRepository.Delete(command.Id);
+                _unitOfWork.Save();
+
+                return true;    
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in Delete user method: ", ex.Message);
+                return false;
+}
+        }
+    }
+}
