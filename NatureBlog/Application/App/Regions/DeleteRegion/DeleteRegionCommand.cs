0a6f255f85f05dd078c8b965ea6a62754f4d9d20
irinaandonova <irinaandonova97@gmail.com>
2022-12-20 16:31:31 +0200

    Modify command

---
+++ NatureBlog/Application/App/Regions/DeleteRegion/DeleteRegionCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace NatureBlog.Application.App.Regions.DeleteRegion
+{
+    public class DeleteRegionCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ NatureBlog/Application/App/Regions/DeleteRegion/DeleteRegionHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using NatureBlog.Application.Repositories;
+
+namespace NatureBlog.Application.App.Regions.DeleteRegion
+{
+    public class DeleteRegionHandler : IRequestHandler<DeleteRegionCommand, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteRegionHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(DeleteRegionCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                _unitOfWork.RegionRepository.Delete(command.Id);
+                await _unitOfWork.Save();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in DeleteRegion method", ex.Message);
+                return false;
+            }
+        }
+    }
+}
