eb8e7a6972c777d5cbb73fc30961cfc3a7539143
irinaandonova <irinaandonova97@gmail.com>
2022-12-20 13:45:19 +0200

    Add methods

--- NatureBlog/Application/Repositories/IDestinationRepository.cs
+++ NatureBlog/Application/Repositories/IDestinationRepository.cs
@@ -15,17 +15,17 @@ namespace NatureBlog.Application.Repositories
         Destination GetDestination(int destinationId);
 
         bool Update(int Id, string name, string description, string imageUrl, Region region);
-        
+
         List<Destination> GetMostVisited();
 
         List<Seaside> GetAllSeasides();
-        
+
         List<HikingTrail> GetAllHikingTrails();
 
         List<Park> GetAllParks();
-        
+
         List<HikingTrail> FilterHikingTrails(int difficulty);
-        
+
         List<Seaside> FilterSeaside(bool isGuarded, bool hasUmbrellas);
 
         List<Park> FilterParks(bool isDogFriendly, bool hasPlayground);
@@ -43,5 +43,9 @@ namespace NatureBlog.Application.Repositories
         bool ChangeDifficulty(int destinationId, int difficulty, int userId);
 
         bool UpdatePlayground(int destinationId, bool hasPlayground);
+
+        HikingTrail GetHikingTrailInfo(int destinationId);
+
+        Park GetParkInfo(int destinationId);
     }
 }
