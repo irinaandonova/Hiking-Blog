bef4494a748a56b4a0801af9a25fd9db57204831
irinaandonova <irinaandonova97@gmail.com>
2022-12-21 14:10:22 +0200

    Add dto

---
+++ NatureBlog/Application/Dto/Destination/Park/ParkHasPlaygroundPutDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NatureBlog.Application.Dto.Destination.Park
+{
+    public class ParkHasPlaygroundPutDto
+    {
+        public int UserId { get; set; }
+        public bool HasPlayground { get; set; }
+    }
+}
