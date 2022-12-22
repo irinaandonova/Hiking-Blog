4292f9e190f4db127a65880a9d3adf0de0482812
irinaandonova <irinaandonova97@gmail.com>
2022-12-22 10:59:04 +0200

    Add dto

---
+++ NatureBlog/Application/Dto/Destination/Destination/DestinationRatePostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NatureBlog.Application.Dto.Destination.Destination
+{
+    public class DestinationRatePostDto
+    {
+        public int UserId { get; set; }
+
+        public int ratingValue { get; set; }
+    }
+}
