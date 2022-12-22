85039cb3acb8bb79bc7252f0a4ab91d6fee20c16
irinaandonova <irinaandonova97@gmail.com>
2022-12-22 16:47:47 +0200

    Add dto

---
+++ NatureBlog/Application/Dto/Destination/HikingTrail/HikingTrailDurationPutDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureBlog.Application.Dto.Destination.HikingTrail
+{
+    public class HikingTrailDurationPutDto
+    {
+        public int UserId { get; set; }
+
+        public int HikingDuration { get; set; }
+    }
+}
