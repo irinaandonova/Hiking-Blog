64bf52299d5057b7f47353d52b2ba184190bf13a
irinaandonova <irinaandonova97@gmail.com>
2022-12-22 16:47:39 +0200

    Add new command

---
+++ NatureBlog/Application/App/Destinations/HikingTrails/Commands/UpdateDuration/UpdateDurationCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace NatureBlog.Application.App.Destinations.HikingTrails.Commands.UpdateDuration
+{
+    public class UpdateDurationCommand: IRequest<bool?>
+    {
+        public int DestinationId { get; set; }
+        
+        public int UserId { get; set; }
+
+        public int HikingDuration { get; set; }
+    }
+}
