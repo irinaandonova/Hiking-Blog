057edc4b371652bb820ffcef8daf2d4538ccd922
irinaandonova <irinaandonova97@gmail.com>
2022-12-29 18:05:16 +0200

    Add profile

---
+++ NatureBlog/Application/Profiles/CommentProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NatureBlog.Application.Dto.Comment;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.Profiles
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            CreateMap<Comment,CommentGetDto>();
+        }
+    }
+}
