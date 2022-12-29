ca78e03f535362c574a79d1988f0337ef1271a27
irinaandonova <irinaandonova97@gmail.com>
2022-12-29 22:06:01 +0200

    Add dto

---
+++ NatureBlog/Application/Dto/Comment/CommentPostDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NatureBlog.Application.Dto.Comment
+{
+    public class CommentPostDto
+    {
+        public int DestinationId { get; set; }
+
+        public int CreatorId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
