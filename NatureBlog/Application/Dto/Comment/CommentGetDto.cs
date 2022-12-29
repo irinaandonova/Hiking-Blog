6f46632db135dc66a18c9793b8e08e159483acd4
irinaandonova <irinaandonova97@gmail.com>
2022-12-29 17:47:29 +0200

    Add dto

---
+++ NatureBlog/Application/Dto/Comment/CommentGetDto.cs
@@ -0,0 +1,13 @@
+﻿namespace NatureBlog.Application.Dto.Comment
+{
+    public class CommentGetDto
+    {
+        public int Id { get; set; }
+
+        public string CreatorUsername { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
