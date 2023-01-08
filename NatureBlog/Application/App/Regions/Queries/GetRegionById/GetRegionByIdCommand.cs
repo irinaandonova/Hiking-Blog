49356c668c00af6e86bf07c6f010c675fad2f16a
irinaandonova <irinaandonova97@gmail.com>
2023-01-08 15:03:43 +0200

    Add query

---
+++ NatureBlog/Application/App/Regions/Queries/GetRegionById/GetRegionByIdCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NatureBlog.Application.Dto.Region;
+
+namespace NatureBlog.Application.App.Regions.Queries.GetRegionById
+{
+    public class GetRegionByIdCommand : IRequest<RegionGetDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ NatureBlog/Application/App/Regions/Queries/GetRegionById/GetRegionByIdHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using NatureBlog.Application.Dto.Region;
+using NatureBlog.Application.Repositories;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.App.Regions.Queries.GetRegionById
+{
+    internal class GetRegionByIdHandler : IRequestHandler<GetRegionByIdCommand, RegionGetDto>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetRegionByIdHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task<RegionGetDto> Handle(GetRegionByIdCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Region region = _unitOfWork.RegionRepository.GetRegion(command.Id);
+                var mappedResult = _mapper.Map<RegionGetDto>(region);
+
+                return Task.FromResult(mappedResult);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception in the Region Get By Id method", ex.Message);
+                return null;
+            }
+
+        }
+
+    }
+}
