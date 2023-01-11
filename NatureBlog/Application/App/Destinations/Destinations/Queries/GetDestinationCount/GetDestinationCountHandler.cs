c699b5bf6a6ccf4dc479325ba37ee1856625de37
irinaandonova <irinaandonova97@gmail.com>
2023-01-11 11:04:25 +0200

    Add method

---
+++ NatureBlog/Application/App/Destinations/Destinations/Queries/GetDestinationCount/GetDestinationCountHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MediatR;
+using NatureBlog.Application.Dto.Destination.Destination;
+using NatureBlog.Application.Exceptions;
+using NatureBlog.Application.Repositories;
+
+namespace NatureBlog.Application.App.Destinations.Destinations.Queries.GetDestinationCount
+{
+    public class GetDestinationCountHandler : IRequestHandler<GetDestinationCountQuery, int?>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetDestinationCountHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task<int?> Handle(GetDestinationCountQuery query, CancellationToken cancellationToken)
+        {
+            try
+            {
+                int count = 0;
+                if (query.Type == "all")
+                    count = _unitOfWork.DestinationRepository.GetAllDestinationsCount();
+                else if (query.Type == "hiking-trail")
+                    count = _unitOfWork.DestinationRepository.GetHikingTrailCount();
+                else if (query.Type == "park")
+                    count = _unitOfWork.DestinationRepository.GetParkCount();
+                else if (query.Type == "seaside")
+                    count = _unitOfWork.DestinationRepository.GetSeasideCount();
+                else
+                    throw new OutOfRangeException("No such type");
+
+                int? result = count / 10 + 1;
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in the GetDestinationCountQuery", ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ NatureBlog/Application/App/Destinations/Destinations/Queries/GetDestinationCount/GetDestinationCountQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using NatureBlog.Application.Dto.Destination.Destination;
+
+namespace NatureBlog.Application.App.Destinations.Destinations.Queries.GetDestinationCount
+{
+    public class GetDestinationCountQuery: IRequest<int?>
+    {
+        public string Type { get; set; }
+    }
+}
