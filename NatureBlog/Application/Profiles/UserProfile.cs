bf2bae4e124c50877a9f35ba14158bd852a32152
irinaandonova <irinaandonova97@gmail.com>
2023-01-16 14:47:55 +0200

    Configure profile

---
+++ NatureBlog/Application/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NatureBlog.Application.Dto.User;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserGetDto>();
+        }
+    }
+}
