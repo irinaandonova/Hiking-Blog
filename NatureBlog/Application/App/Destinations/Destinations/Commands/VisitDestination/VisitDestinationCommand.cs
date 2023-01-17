f642a92d8671fdd888e638750ca32ac56eeecb88
irinaandonova <irinaandonova97@gmail.com>
2023-01-17 14:23:56 +0200

    Add command

---
+++ NatureBlog/Application/App/Destinations/Destinations/Commands/VisitDestination/VisitDestinationCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.App.Destinations.Destinations.Commands.VisitDestination
+{
+    public class VisitDestinationCommand : IRequest<List<User>>
+    {
+        public int DestinationId { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ NatureBlog/Application/App/Destinations/Destinations/Commands/VisitDestination/VisitDestinationHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using NatureBlog.Application.Repositories;
+using NatureBlog.Domain.Models;
+
+namespace NatureBlog.Application.App.Destinations.Destinations.Commands.VisitDestination
+{
+    public class VisitDestinationHandler : IRequestHandler<VisitDestinationCommand, List<User>>
+    {
+        private readonly IUnitOfWork _unitOfwork;
+
+        public VisitDestinationHandler(IUnitOfWork unitOfwork)
+        {
+            _unitOfwork = unitOfwork;
+        }
+
+        public List<User> Handle(VisitDestinationCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                /*
+                 Destination destination = _unitOfwork.DestinationRepository.GetDestination(command.DestinationId);
+                if (destination is null || user is null)
+                    return null;
+                */
+                User user = _unitOfwork.UserRepository.GetUser(command.UserId);
+
+               List<User> visitors= _unitOfwork.DestinationRepository.VisitDestination(user, command.DestinationId);
+                //await _unitOfwork.Save();
+                
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in the Visit Destination Method", ex);
+                return new List<User>  { };
+            }
+        }
+    }
+}
