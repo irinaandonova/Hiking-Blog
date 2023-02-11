95fe445d34b7fa8090ec3feab7994f193f86dce4
irinaandonova <irinaandonova97@gmail.com>
2023-02-11 15:17:22 +0200

    Handle exceptions

--- NatureBlog/Application/App/Comments/Commands/CreateComment/CreateCommentHandler.cs
+++ NatureBlog/Application/App/Comments/Commands/CreateComment/CreateCommentHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using NatureBlog.Application.Dto.Comment;
+using NatureBlog.Application.Exceptions;
 using NatureBlog.Application.Repositories;
 using NatureBlog.Domain.Models;
 
@@ -21,10 +22,16 @@ namespace NatureBlog.Application.App.Comments.Commands.CreateComment
         {
             try
             {
-                Comment comment = new Comment { DestinationId = command.DestinationId, CreatorId = command.CreatorId, Text = command.Text, Date = DateTime.Now };
+                User user = _unitOfWork.UserRepository.GetUser(command.CreatorId);
+                if (user is null)
+                    throw new UserNotFoundException("No user with this id found!");
 
+                Comment comment = new Comment { DestinationId = command.DestinationId, CreatorId = command.CreatorId, Text = command.Text, Date = DateTime.Now };
                 _unitOfWork.CommentRepository.CreateComment(comment);
+
                 var destination = _unitOfWork.DestinationRepository.GetDestination(comment.DestinationId);
+                if (destination is null)
+                    throw new DestinationNotFoundException("No destination with this id found"!);
 
                 destination.Comments.Add(comment);
                 await _unitOfWork.Save();
@@ -32,11 +39,24 @@ namespace NatureBlog.Application.App.Comments.Commands.CreateComment
                 if (comment is not null)
                 {
                     var mappedResult = _mapper.Map<CommentGetDto>(comment);
+                    mappedResult.Username = user.Username;
                     return mappedResult;
                 }
 
                 else
                     return null;
+            }
+            catch(UserNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch(DestinationNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch(CommentCreationFailedException ex)
+            {
+                throw ex;
             }
             catch (Exception ex)
             {
