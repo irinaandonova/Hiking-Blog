1741d0174325102d472e79b8056d06956c03bdcd
irinaandonova <irinaandonova97@gmail.com>
2023-02-11 15:17:46 +0200

    Add new custom exception

---
+++ NatureBlog/Application/Exceptions/CommentCreationFailedException.cs
@@ -0,0 +1,19 @@
+﻿namespace NatureBlog.Application.Exceptions
+{
+    public class CommentCreationFailedException: Exception
+    {
+        public CommentCreationFailedException()
+        {
+        }
+
+        public CommentCreationFailedException(string message)
+                : base(message)
+        {
+        }
+
+        public CommentCreationFailedException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
