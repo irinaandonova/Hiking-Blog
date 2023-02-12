bff69e2d4ac5a381937e169997fcaf37c6a9babb
irinaandonova <irinaandonova97@gmail.com>
2023-02-12 14:09:19 +0200

    Rethrow exceptions

--- NatureBlog/Application/App/Comments/Commands/DeleteComment/DeleteCommentHandler.cs
+++ NatureBlog/Application/App/Comments/Commands/DeleteComment/DeleteCommentHandler.cs
@@ -5,7 +5,7 @@ using NatureBlog.Domain.Models;
 
 namespace NatureBlog.Application.App.Comments.Commands.DeleteComment
 {
-    public class DeleteCommentHandler : IRequestHandler<DeleteCommentCommand, bool?>
+    public class DeleteCommentHandler : IRequestHandler<DeleteCommentCommand, bool>
     {
         private readonly IUnitOfWork _unitOfWork;
         public DeleteCommentHandler(IUnitOfWork unitOfWork)
@@ -13,27 +13,50 @@ namespace NatureBlog.Application.App.Comments.Commands.DeleteComment
             _unitOfWork= unitOfWork;
         }
 
-        public async Task<bool?> Handle(DeleteCommentCommand command, CancellationToken cancellationToken)
+        public async Task<bool> Handle(DeleteCommentCommand command, CancellationToken cancellationToken)
         {
             try
             {
                 Destination destination = _unitOfWork.DestinationRepository.GetDestination(command.DestinationId);
                 Comment comment = _unitOfWork.CommentRepository.GetComment(command.CommentId);
-                
-                if (destination is null || comment is null)
-                    return null;
+
+                if (destination is null)
+                    throw new DestinationNotFoundException("No destination with given id found!");
+
+                if (comment is null)
+                    throw new CommentNotFoundException("No comment with given id!");
 
                 if (comment.CreatorId != command.CreatorId)
-                    return null;
+                    throw new UserNotCreatorException("Current user is not the creator of the comment!");
 
-                destination.Comments.Remove(comment);
+                bool isRemoved = destination.Comments.Remove(comment);
+                if(!isRemoved)
+                {
+                    throw new ModificationFailedException("Comment unsuccessfully removed from destination comments array!")
+                }
                 bool response = _unitOfWork.CommentRepository.DeleteComment(command.DestinationId, command.CommentId);
                 await _unitOfWork.Save();
 
                 if (!response)
-                    throw new ModificationFailedException("Creating a comment was unsuccessful!");
+                    throw new ModificationFailedException("Saving changes was unsuccessful!");
                 
                 return true;
+            }
+            catch(CommentNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch(ModificationFailedException ex)
+            {
+                throw ex;
+            }
+            catch (UserNotCreatorException ex)
+            {
+                throw ex;
+            }
+            catch (DestinationNotFoundException ex)
+            {
+                throw ex;
             }
             catch (Exception ex)
             {
--- NatureBlog/Application/App/Comments/Commands/EditComment/EditCommmentHandler.cs
+++ NatureBlog/Application/App/Comments/Commands/EditComment/EditCommmentHandler.cs
@@ -1,11 +1,12 @@
 ﻿using AutoMapper;
 using MediatR;
+using NatureBlog.Application.Exceptions;
 using NatureBlog.Application.Repositories;
 using NatureBlog.Domain.Models;
 
 namespace NatureBlog.Application.App.Comments.Commands.EditComment
 {
-    public class EditCommmentHandler : IRequestHandler<EditCommentCommand, bool?>
+    public class EditCommmentHandler : IRequestHandler<EditCommentCommand, bool>
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
@@ -16,21 +17,38 @@ namespace NatureBlog.Application.App.Comments.Commands.EditComment
             _mapper = mapper;
         }
 
-        public async Task<bool?> Handle(EditCommentCommand command, CancellationToken cancellationToken)
+        public async Task<bool> Handle(EditCommentCommand command, CancellationToken cancellationToken)
         {
             try
             {
                 Comment comment = _unitOfWork.CommentRepository.GetComment(command.Id);
 
-                if (comment is null || comment.CreatorId != command.UserId)
-                    return null;
+                if (comment is null )
+                    throw new CommentNotFoundException("No comment with given id found!");
+
+                if (comment.CreatorId != command.UserId)
+                    throw new UserNotCreatorException("Current user is not the creator of this comment");
 
                 var result = _unitOfWork.CommentRepository.EditComment(comment, command.Text);
                 await _unitOfWork.Save();
+                if (!result)
+                    throw new CommentModificationFailedException("Comment wasn't successfully modified");
 
                 return true;
             }
-            catch(Exception ex)
+            catch(CommentNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (CommentModificationFailedException ex)
+            {
+                throw ex;
+            }
+            catch (UserNotCreatorException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine("Exception in Edit Comment Method: ", ex.Message);
                 return false;
